44868c7b5704c8d2ea372648afa5161472daf332
Alexandra <55248127+randomartlover@users.noreply.github.com>
2020-07-29 17:57:22 +0100

    jsonplaceholder added

--- src/App.vue
+++ src/App.vue
@@ -10,29 +10,15 @@
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos.vue";
 import AddTodo from "./components/AddTodo";
+import axios from "axios";
+
 export default {
   name: "App",
   components: { Header, Todos, AddTodo },
 
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false,
-        },
-      ],
+      todos: [],
     };
   },
   methods: {
@@ -40,8 +26,22 @@ export default {
       this.todos = this.todos.filter((todo) => todo.id !== id);
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+      axios
+        .post("http://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed,
+        })
+        .then((res) => (this.todos = [...this.todos, res.data]))
+        .catch((err) => console.log(err));
     },
+  },
+
+  created() {
+    axios
+      .get("http://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((res) => (this.todos = res.data))
+      .catch((err) => console.log(err));
   },
 };
 </script>
